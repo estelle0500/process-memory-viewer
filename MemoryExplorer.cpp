a4e197f4ebd861ebc5e1f9f9d1d4c1512f54755d
Tyler <tylervbarton@gmail.com>
2020-10-10 16:23:17 -0500

    Initial Commit

---
+++ MemoryExplorer.cpp
@@ -0,0 +1,9 @@
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>         // For getopt
+
+#define PROC_DIRECTORY   "/proc/"
+
+int main(int argc, char* argv[]) {
+    cout << "hello world\n";
+}
