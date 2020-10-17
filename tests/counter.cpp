1b7e26dfe51caf4c9ba6929e7d63fefced5791de
tyler <tylervbarton@gmail.com>
2020-10-17 03:40:30 -0500

    modify counter test to finite iterations to avoid zombie process

--- tests/counter.cpp
+++ tests/counter.cpp
@@ -1,7 +1,8 @@
 #include <unistd.h>
 #include <iostream>
 
-#define SLEEP_TIME 1000
+#define SLEEP_TIME 1
+#define DEBUG
 
 /* 
 This test program increments a non-static integer value infinitely
@@ -9,9 +10,11 @@ This test program increments a non-static integer value infinitely
 
 int main() {
     int value = 0;
-    while(1){
+    while(value < 100){
+#ifdef DEBUG
+        if(value % 10 == 0) std::cout << value << std::endl;
+#endif
         value++;
-        std::cout << value << std::endl;
         sleep(SLEEP_TIME);
     }
 }
