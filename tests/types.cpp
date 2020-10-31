938aef7e6a8191d3c44a3b973faa9a8be6fbb516
tyler <tylervbarton@gmail.com>
2020-10-31 02:07:23 -0500

    add types.cpp test to search for different value types in memory.

---
+++ tests/types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+This test program will run in an infinite loop.
+Our ProcessMemoryViewer should be able to overwrite the `status` variable and break out of the loop.
+*/
+int main() {
+    static int l    = 1336;
+    float f1        = 1337.0;
+    float f2        = 1337.5;
+    int leet        = 1337;
+    double l33t     = 1337;
+
+    std::cout << "Types started: find 1337!" << std::endl;
+}
