32616ccf5b60822bfb8bd44c48c3c14d7c63f267
estelle0500 <estelle0500@gmail.com>
2020-11-14 18:32:12 +0800

    Add run and single step tracing modes

--- src/ProcessTracer.cpp
+++ src/ProcessTracer.cpp
@@ -3,20 +3,62 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
 #include <unistd.h>
 #include <iostream>
 #include <signal.h>
+#include <sstream>
 
 namespace ProcessMemoryViewer {
 void ProcessTracer::Start(char *executable_name, char **args) {
     int fork_code = fork();
     if (fork_code == 0) { // Child
+        std::cout << "Trace me" << std::endl;
+        ptrace(PTRACE_TRACEME, 0, NULL, 0);
         execvp(executable_name, args);
         exit(1);
     }
 
     pid_ = fork_code;
+    int status;
+    if (waitpid(pid_, &status, 0) == -1) {
+        perror("Couldn't wait on child process");
+        exit(1);
+    }
     std::cout << "Process started with PID: " << pid_ << std::endl;
+}
+
+void ProcessTracer::Run() {
+    int status = 1407;
+    int in_call = 0;
+    struct user_regs_struct regs;
+
+    while (status == 1407) {
+        ptrace(PTRACE_GETREGS, pid_, NULL, &regs) == -1;
+        if (!in_call) {
+            printf("SystemCall %llu called with %llu, %llu, %llu\n",regs.orig_rax, regs.rbx, regs.rcx, regs.rdx);
+        }
+        in_call = 1 - in_call; 
+        
+        if (ptrace(PTRACE_SYSCALL, pid_, NULL, NULL) == -1) {
+            break;
+        }
+        if (waitpid(pid_, &status, 0) == -1) {
+            break;
+        }
+    }
+}
+
+void ProcessTracer::SingleStep(size_t num_steps) {
+    for (size_t i = 0; i < num_steps; ++i) {
+        if (ptrace(PTRACE_SINGLESTEP, pid_, NULL, 0) == -1) {
+            perror("Ptrace single step failed");
+        }
+
+        int status;
+        waitpid(pid_, &status, 0);
+    }
 }
 
 bool ProcessTracer::IsValid() const {
@@ -29,9 +71,9 @@ bool ProcessTracer::IsRunning() const {
     }
     
     struct stat status;
-    if (stat((("/proc/" + pid_)), &status) != -1) {
-        return false;
-    }
+    std::ostringstream path;
+    path << "/proc/" << pid_;
+    return stat(path.str().c_str(), &status) != -1;
 }
 
 void ProcessTracer::Kill() {
