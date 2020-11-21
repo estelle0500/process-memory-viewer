6cdf57a50b7227be7380100e5cbe21fa39c526c7
tyler <tylervbarton@gmail.com>
2020-11-21 04:35:52 -0600

    add address display to counter test for easier debugging

--- tests/counter.cpp
+++ tests/counter.cpp
@@ -10,8 +10,11 @@ This test program increments a non-static integer value to a set iteration
 */
 
 int main() {
-    sleep(DELAY);
+
     int value = 1;
+
+    std::cout << "Counter started. Address of value is " << &value << std::endl;
+    sleep(DELAY);
     while(value < 100){
 #ifdef DEBUG
         if(value % 10 == 0) std::cout << value << std::endl;
